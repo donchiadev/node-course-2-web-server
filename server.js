0db19309f7a473d2b88bd11cb382ee07284141d3
Antonio D'Onchia <donchiadeveloper@gmail.com>
2017-05-02 09:13:59 +0200

    Some changes in server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 hbs.registerPartials(__dirname + '/views/partials');
 
